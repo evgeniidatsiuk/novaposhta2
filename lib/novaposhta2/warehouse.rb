c9596069eb81d4f9abbaca9731ee3f3b355b5c10
Eugene Datsiuk <evgenii.datsiuk@gmail.com>
2021-11-14 19:21:19 +0200

    Fix warehouse attributes.

--- lib/novaposhta2/warehouse.rb
+++ lib/novaposhta2/warehouse.rb
@@ -1,14 +1,14 @@
 module Novaposhta2
   # Represents a warehouse.
   class Warehouse < Base
-    attr_reader :description, :description_ru, :ref, :number, :longtitude, :latitude #:nodoc:
+    attr_reader :description, :description_ru, :ref, :number, :longitude, :latitude #:nodoc:
 
     def initialize(params) #:nodoc:
       @description_ru = params['DescriptionRu']
       @description    = params['Description']
       @ref            = params['Ref']
       @number         = params['Number'].to_i
-      @longtitude     = params['Longtitude']
+      @longitude      = params['Longitude']
       @latitude       = params['Latitude']
     end
   end
